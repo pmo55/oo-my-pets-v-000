f9af6433bc5d86065acab6d11d6ba8527f9da0b7
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-24 23:41:27 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,20 @@
 class Owner
-  # code goes here
+  attr_accessor :name
+@@all=[]
+def initialize(name)
+  @name=name
+  @@all << self
+end
+def self.all
+  @@all
+end
+def self.count
+  @@all.count
+end
+def self.reset_all
+  @@all.clear
+end
+def species
+  self.species=species
+end
 end
